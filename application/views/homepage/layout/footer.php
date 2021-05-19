f4d9c73542bc7fe89f4529652ad4dfc54c7afa4f
ngthluu <luu.nguyen2101@hcmut.edu.vn>
2021-05-19 21:20:39 +0700

    Fix menu

--- application/views/homepage/layout/footer.php
+++ application/views/homepage/layout/footer.php
@@ -1,68 +1,68 @@
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
 
-<div class="footer-top">
-    <div class="container">
-        <div class="row gy-4">
-            <div class="col-lg-5 col-md-12 footer-info">
-                <a href="<?= site_url() ?>" class="logo d-flex align-items-center">
-                    <img src="<?= base_url("resources/logo.jpg") ?>" alt="">
-                    <span><?= PROJECT_NAME ?></span>
-                </a>
-                <p>New style restaurant.</p>
-                <p>Fast, convenient & delicious.</p>
-                <div class="social-links mt-3">
-                    <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                    <a href="#" class="instagram"><i class="bi bi-instagram bx bxl-instagram"></i></a>
-                    <a href="#" class="linkedin"><i class="bi bi-linkedin bx bxl-linkedin"></i></a>
+    <div class="footer-top">
+        <div class="container">
+            <div class="row gy-4">
+                <div class="col-lg-5 col-md-12 footer-info">
+                    <a href="<?= site_url() ?>" class="logo d-flex align-items-center">
+                        <img src="<?= base_url("resources/logo.jpg") ?>" alt="">
+                        <span><?= PROJECT_NAME ?></span>
+                    </a>
+                    <p>New style restaurant.</p>
+                    <p>Fast, convenient & delicious.</p>
+                    <div class="social-links mt-3">
+                        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bi bi-instagram bx bxl-instagram"></i></a>
+                        <a href="#" class="linkedin"><i class="bi bi-linkedin bx bxl-linkedin"></i></a>
+                    </div>
                 </div>
+
+                <div class="col-lg-2 col-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="bi bi-chevron-right"></i> <a href="<?= site_url() ?>">Home</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="<?= site_url("contact-us") ?>">Contact us</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="<?= site_url("privacy-policy") ?>">Privacy policy</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-6 footer-links">
+                    <h4>Our Branches</h4>
+                    <ul>
+                        <?php
+                        $CI = &get_instance();
+                        $CI->load->model("M_Branch");
+                        $branch_list = $CI->M_Branch->gets_all(["status" => M_Branch::STATUS_ACTIVE]);
+                        foreach ($branch_list as $branch) {
+                        ?>
+                            <li><i class="bi bi-chevron-right"></i> <a href="#"><?= $branch->name ?></a></li>
+                        <?php } ?>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+                    <h4>Contact Us</h4>
+                    <p>
+                        Khoi 6, Dak To<br>
+                        Dak To, Kon Tum<br>
+                        Viet Nam <br><br>
+                        <strong>Phone:</strong> <?= PHONE ?><br>
+                        <strong>Email:</strong> admin@<?= EMAIL_PATH ?><br>
+                    </p>
+
+                </div>
+
             </div>
-
-            <div class="col-lg-2 col-6 footer-links">
-                <h4>Useful Links</h4>
-                <ul>
-                    <li><i class="bi bi-chevron-right"></i> <a href="<?= site_url() ?>">Home</a></li>
-                    <li><i class="bi bi-chevron-right"></i> <a href="<?= site_url("contact-us") ?>">Contact us</a></li>
-                    <li><i class="bi bi-chevron-right"></i> <a href="<?= site_url("privacy-policy") ?>">Privacy policy</a></li>
-                </ul>
-            </div>
-
-            <div class="col-lg-2 col-6 footer-links">
-                <h4>Our Branches</h4>
-                <ul>
-                <?php 
-                $CI = &get_instance();
-                $CI->load->model("M_Branch");
-                $branch_list = $CI->M_Branch->gets_all(["status" => M_Branch::STATUS_ACTIVE]);
-                foreach ($branch_list as $branch) {
-                ?>
-                    <li><i class="bi bi-chevron-right"></i> <a href="#"><?= $branch->name ?></a></li>
-                <?php } ?>
-                </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-                <h4>Contact Us</h4>
-                <p>
-                    Khoi 6, Dak To<br>
-                    Dak To, Kon Tum<br>
-                    Viet Nam <br><br>
-                    <strong>Phone:</strong> <?= PHONE ?><br>
-                    <strong>Email:</strong> admin@<?= EMAIL_PATH ?><br>
-                </p>
-
-            </div>
-
         </div>
     </div>
-</div>
 
-<div class="container">
-    <div class="copyright">
-        &copy; Copyright <strong><span><?= PROJECT_COPYRIGHT_BY ?></span></strong>. All Rights Reserved
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong><span><?= PROJECT_COPYRIGHT_BY ?></span></strong>. All Rights Reserved
+        </div>
     </div>
-</div>
 </footer>
 <!-- End Footer -->
 
@@ -72,13 +72,158 @@
 <script src="<?= base_url("assets/homepage/js/popper.min.js") ?>"></script>
 <script src="<?= base_url("assets/homepage/js/bootstrap.min.js") ?>"></script>
 <script>
-$(document).on('scroll', () => {
-    if (window.scrollY > 100) {
-        $('.back-to-top').addClass('active')
-    } else {
-        $('.back-to-top').removeClass('active')
-    }
-});
+    (function() {
+        "use strict";
+
+        /**
+         * Easy selector helper function
+         */
+        const select = (el, all = false) => {
+            el = el.trim()
+            if (all) {
+                return [...document.querySelectorAll(el)]
+            } else {
+                return document.querySelector(el)
+            }
+        }
+
+        /**
+         * Easy event listener function
+         */
+        const on = (type, el, listener, all = false) => {
+            if (all) {
+                select(el, all).forEach(e => e.addEventListener(type, listener))
+            } else {
+                select(el, all).addEventListener(type, listener)
+            }
+        }
+
+        /**
+         * Easy on scroll event listener 
+         */
+        const onscroll = (el, listener) => {
+            el.addEventListener('scroll', listener)
+        }
+
+        /**
+         * Navbar links active state on scroll
+         */
+        let navbarlinks = select('#navbar .scrollto', true)
+        const navbarlinksActive = () => {
+            let position = window.scrollY + 200
+            navbarlinks.forEach(navbarlink => {
+                if (!navbarlink.hash) return
+                let section = select(navbarlink.hash)
+                if (!section) return
+                if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                    navbarlink.classList.add('active')
+                } else {
+                    navbarlink.classList.remove('active')
+                }
+            })
+        }
+        window.addEventListener('load', navbarlinksActive)
+        onscroll(document, navbarlinksActive)
+
+        /**
+         * Scrolls to an element with header offset
+         */
+        const scrollto = (el) => {
+            let header = select('#header')
+            let offset = header.offsetHeight
+
+            if (!header.classList.contains('header-scrolled')) {
+                offset -= 10
+            }
+
+            let elementPos = select(el).offsetTop
+            window.scrollTo({
+                top: elementPos - offset,
+                behavior: 'smooth'
+            })
+        }
+
+        /**
+         * Toggle .header-scrolled class to #header when page is scrolled
+         */
+        let selectHeader = select('#header')
+        if (selectHeader) {
+            const headerScrolled = () => {
+                if (window.scrollY > 100) {
+                    selectHeader.classList.add('header-scrolled')
+                } else {
+                    selectHeader.classList.remove('header-scrolled')
+                }
+            }
+            window.addEventListener('load', headerScrolled)
+            onscroll(document, headerScrolled)
+        }
+
+        /**
+         * Back to top button
+         */
+        let backtotop = select('.back-to-top')
+        if (backtotop) {
+            const toggleBacktotop = () => {
+                if (window.scrollY > 100) {
+                    backtotop.classList.add('active')
+                } else {
+                    backtotop.classList.remove('active')
+                }
+            }
+            window.addEventListener('load', toggleBacktotop)
+            onscroll(document, toggleBacktotop)
+        }
+
+        /**
+         * Mobile nav toggle
+         */
+        on('click', '.mobile-nav-toggle', function(e) {
+            select('#navbar').classList.toggle('navbar-mobile')
+            this.classList.toggle('bi-list')
+            this.classList.toggle('bi-x')
+        })
+
+        /**
+         * Mobile nav dropdowns activate
+         */
+        on('click', '.navbar .dropdown > a', function(e) {
+            if (select('#navbar').classList.contains('navbar-mobile')) {
+                e.preventDefault()
+                this.nextElementSibling.classList.toggle('dropdown-active')
+            }
+        }, true)
+
+        /**
+         * Scrool with ofset on links with a class name .scrollto
+         */
+        on('click', '.scrollto', function(e) {
+            if (select(this.hash)) {
+                e.preventDefault()
+
+                let navbar = select('#navbar')
+                if (navbar.classList.contains('navbar-mobile')) {
+                    navbar.classList.remove('navbar-mobile')
+                    let navbarToggle = select('.mobile-nav-toggle')
+                    navbarToggle.classList.toggle('bi-list')
+                    navbarToggle.classList.toggle('bi-x')
+                }
+                scrollto(this.hash)
+            }
+        }, true)
+
+        /**
+         * Scroll with ofset on page load with hash links in the url
+         */
+        window.addEventListener('load', () => {
+            if (window.location.hash) {
+                if (select(window.location.hash)) {
+                    scrollto(window.location.hash)
+                }
+            }
+        });
+    })();
 </script>
 </body>
+
 </html>
