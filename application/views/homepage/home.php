918fbb75d796d65a1c0a175a3c8150644b762598
ngthluu <luu.nguyen2101@hcmut.edu.vn>
2021-05-19 19:55:26 +0700

    Finish homepage

--- application/views/homepage/home.php
+++ application/views/homepage/home.php
@@ -1,6 +1,23 @@
 <main id="main">
-    <div class="container">
-    
+    <div class="container d-flex h-100">
+        <div class="justify-content-center align-self-center">
+            <h1 class="page-title t-white font-weight-bold">
+                New style restaurant
+            </h1>
+            <h3 class="t-yellow mt-3">
+                Fast, convenient & delicious.
+            </h3>
+            <div class="t-white mt-3">
+                Create account, scan QR Code and have a delicious meal.
+            </div>
+            <div class="mt-5">
+                <button class="btn btn-danger" href="#">Sign up here > </button>
+            </div>
+            <div class="mt-3">
+                <div class="t-white">Already have account ?</div>
+                <div class="t-red">Sign in here</div>
+            </div>
+        </div>
     </div>
 </main>
 
