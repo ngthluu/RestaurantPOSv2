4d0b70e5acff54424b81dd38869d49d24cf932bd
ngthluu <luu.nguyen2101@hcmut.edu.vn>
2021-05-20 12:44:37 +0700

    Finish order page

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -9,6 +9,8 @@ class Order extends SITE_Controllers {
 		if (!is_logged_in()) {
 			redirect(site_url());
 		}
+
+        $this->load->model("M_Menu");
 	}
 
 	public function index($branch_id=null, $table_num=null) {
@@ -25,6 +27,12 @@ class Order extends SITE_Controllers {
         
         $data["main_header"] = "Our menu";
         $data["main_view"] = "homepage/order";
+
+        $data["menu_list"] = $this->M_Menu->gets_all([
+            "branch" => $branch_id,
+            "status" => M_Menu::STATUS_PUBLISHED,
+            "status_date" => M_Menu::STATUS_DATE_AVAILABLE
+        ]);
 
 		$this->load->view("homepage/layout/main", $data);
 	}
--- application/views/homepage/order.php
+++ application/views/homepage/order.php
@@ -1,54 +1,36 @@
-<main id="main" class="menu-list">
-    <div class="container d-flex h-100">
-        <div class="justify-content-center align-self-center">
-            <h1 class="page-title t-white font-weight-bold">
-                Our menu
-            </h1>
-            <div class="row gy-4">
-                <div class="col-md-6">
-                    <div class="menu-box">
-                        <i class="bi bi-geo-alt"></i>
-                        <h3>Address</h3>
-                        <p>Khoi 6, Dak To, Dak To,<br>Kon Tum, Viet Nam</p>
+<main id="main" class="d-flex flex-column justify-content-start menu-list">
+    <div class="container">
+        <h1 class="page-title t-white font-weight-bold">
+            Our menu
+        </h1>
+        <?php if (!empty($menu_list)) { ?>
+        <div class="row gy-4">
+        <?php foreach ($menu_list as $menu) { ?>
+            <div class="col-md-6 mb-3">
+                <div class="menu-box d-flex flex-column justify-content-between"
+                style="
+                background:url(<?= $menu->image ? base_url("resources/menu/".$menu->id."/".$menu->image) : base_url("resources/no-image.jpg") ?>);
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+                box-shadow: inset 0 0 0 2000px rgba(0, 0, 0, 0.5);
+                "> 
+                    <div>
+                        <h3><?= $menu->name ?></h3>
+                        <p class="price"><?= number_format($menu->price) ?>đ</p>
                     </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="menu-box">
-                        <i class="bi bi-telephone"></i>
-                        <h3>Call Us</h3>
-                        <p>+038 347 7379<br>+038 347 7370</p>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="menu-box">
-                        <i class="bi bi-envelope"></i>
-                        <h3>Email Us</h3>
-                        <p>admin@<?= EMAIL_PATH ?><br>contact@<?= EMAIL_PATH ?></p>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="menu-box">
-                        <i class="bi bi-clock"></i>
-                        <h3>Open Hours</h3>
-                        <p>Monday - Friday<br>07:00 AM - 09:00 PM</p>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="menu-box">
-                        <i class="bi bi-clock"></i>
-                        <h3>Open Hours</h3>
-                        <p>Monday - Friday<br>07:00 AM - 09:00 PM</p>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="menu-box">
-                        <i class="bi bi-clock"></i>
-                        <h3>Open Hours</h3>
-                        <p>Monday - Friday<br>07:00 AM - 09:00 PM</p>
+                    <p><?= word_limiter($menu->description, 30) ?></p>
+                    <div class="d-flex justify-content-end">
+                        <input type="number" class="mr-2 text-center" value="0">
+                        <button class="btn btn-danger"><i class="bi bi-cart"></i></button>
                     </div>
                 </div>
             </div>
+        <?php } ?>
         </div>
+        <?php } else { ?>
+        <div class="t-yellow mt-3">The menu is currently empty.</div> 
+        <?php } ?>
     </div>
 </main>
 
