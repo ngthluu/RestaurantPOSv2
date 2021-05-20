da04d936ac593f9aa5d9e5acf97bd81a3de62912
ngthluu <luu.nguyen2101@hcmut.edu.vn>
2021-05-20 09:23:32 +0700

    Finish load data to account settings

--- application/controllers/Settings.php
+++ application/controllers/Settings.php
@@ -16,6 +16,17 @@ class Settings extends SITE_Controllers {
 	public function index() {
         $data["main_header"] = "Settings";
         $data["main_view"] = "homepage/settings";
+
+        $data["customer"] = $this->M_Customer->get($_SESSION["uid"]);
+
 		$this->load->view("homepage/layout/main", $data);
 	}
+
+    public function check_form() {
+
+    }
+
+    public function save() {
+
+    }
 }
